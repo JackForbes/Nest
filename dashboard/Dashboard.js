36dd4aea168123ed51a1af8483ab630fdf448ef6
JackForbes <jackforbes24@gmail.com>
2015-06-14 15:04:28 -0400

    First Commit

---
+++ dashboard/Dashboard.js
@@ -0,0 +1,8 @@
+(function(){
+  'use strict';
+
+  // Prepare the 'dashboard' module for subsequent registration of controllers and delegates
+  angular.module('dashboard', [ 'ngMaterial' ]);
+
+
+})();
